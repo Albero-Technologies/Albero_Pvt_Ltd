4d72156fd34c646d42f543bf74a44b2c370e12a5
harshit katiyar <115411554+Lucifer7344@users.noreply.github.com>
2026-02-16 13:28:06 +0530

    Update contact phone number

--- client/src/constants/contact.ts
+++ client/src/constants/contact.ts
@@ -17,7 +17,7 @@ export const contactData = {
     card: {
         title: 'Get in touch',
         email: 'contact@albero.in',
-        phone: '+91 9170780671',
+        phone: '+91 8172963607',
         address: 'Noida, India'
     },
     link: {
