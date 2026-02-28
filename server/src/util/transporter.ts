5bd6aec5c7c1d1ee07d8d3766c4ddf3341c6858c
Anuj3553 <anujverma3553@gmail.com>
2026-03-01 03:26:22 +0530

    refactor: update nodemailer transport configuration for improved connection settings

--- server/src/util/transporter.ts
+++ server/src/util/transporter.ts
@@ -2,9 +2,14 @@ import nodemailer from 'nodemailer'
 import config from '../config/config'
 
 export const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // TLS
     auth: {
         user: config.EMAIL_USER,
         pass: config.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 })
