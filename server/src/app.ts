9f355e3d741178376fe414c55fb0b8ef772f123b
Anuj3553 <anujverma3553@gmail.com>
2026-03-01 03:52:03 +0530

    refactor: update email sending logic and configuration in contact controller and transporter

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,6 +20,7 @@ app.use(
         credentials: true
     })
 )
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../', 'public')))
 
--- server/src/util/transporter.ts
+++ server/src/util/transporter.ts
@@ -2,14 +2,9 @@ import nodemailer from 'nodemailer'
 import config from '../config/config'
 
 export const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // TLS
+    service: 'gmail',
     auth: {
         user: config.EMAIL_USER,
         pass: config.EMAIL_PASS
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
+    }
 })
